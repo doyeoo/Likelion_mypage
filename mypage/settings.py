6e53a2ab6740bfd852db61113df6ee9ae9cef450
doyeoo <ehdusla013@gmail.com>
2021-07-21 19:22:14 +0900

    dj_db

--- mypage/settings.py
+++ mypage/settings.py
@@ -15,9 +15,8 @@ import os
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
